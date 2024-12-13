4c35007c7ef1df407ff463fa5070b24aebd2ccab
kimjaeik95 <wodlrdlsla@naver.com>
2024-12-13 18:20:25 +0900

    first commit

---
+++ src/main/java/com/example/onemoreboard/OnemoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.onemoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onemoreboard/OnemoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onemoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
